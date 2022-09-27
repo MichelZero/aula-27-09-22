7bd4d8534ef9cc09eb3b43a30e31d9bf4dc53ea9
Michel <106674127+MichelZero@users.noreply.github.com>
2022-09-27 10:24:05 -0300

    up

---
+++ ex02.py
@@ -0,0 +1,25 @@
+#
+#
+#
+# version 3.9 (IF - ELIF - ELSE)
+# version 3.10 (match-CASE)
+
+a = int(input("informe o valo de de A:"))
+b = int(input("informe o valo de de B:"))
+
+if (a < b):
+  print("valor1")
+
+
+if (a > b):
+  print("valor1")
+elif (b > a):
+  print("valor2")
+  
+  
+if (a > b):
+  print("valor1")
+elif (b > a):
+  print("valor2")
+else:
+  print("valor3")
