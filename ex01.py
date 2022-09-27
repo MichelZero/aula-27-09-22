e145b1284040dcb6e61fd21b669c52c0c6446ac4
Michel <106674127+MichelZero@users.noreply.github.com>
2022-09-27 09:56:12 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,16 @@
+#
+#
+# data 27/09/22
+
+# tipo dados
+valor1 = int(input("informe um valor inteiro: "))
+print(f"o valor1 = {valor1}, é tipo {type(valor1)}")
+
+valor2 = float(input("informe um valor real: "))
+print(f"o valor2 = {valor2}, é tipo {type(valor2)}")
+
+valor3 = input("informe um valor string: ")
+print(f"o valor3 = {valor3}, é tipo {type(valor3)}")
+
+valor4 = valor1 >= valor2
+print(f"o valor4 = {valor4}, é tipo {type(valor4)}")
